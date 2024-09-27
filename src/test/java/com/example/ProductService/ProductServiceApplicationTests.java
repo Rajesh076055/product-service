3368922bc98ae91564bc90f55cce0286eb15caf4
Rajes076055 <imrajeshneupane@gmail.com>
2024-09-27 16:40:10 -0400

    Product service updated on Sep 27, 2024 4:40 PM

--- src/test/java/com/example/ProductService/ProductServiceApplicationTests.java
+++ src/test/java/com/example/ProductService/ProductServiceApplicationTests.java
@@ -1,20 +1,80 @@
 package com.example.ProductService;
 
-import org.junit.jupiter.api.Test;
+import com.example.ProductService.DTO.ProductRequest;
+import com.example.ProductService.Repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ProductServiceApplicationTests {
 
 	@Container
-	MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private ProductRepository productRepository;
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
-	void contextLoads() {
+	@Order(1)
+	void createProductTest() throws Exception {
+		ProductRequest productRequest = getProductRequest();
+		String productRequestString = objectMapper.writeValueAsString(productRequest);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/product")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(productRequestString))
+				.andExpect(status().isCreated());
+
+        Assertions.assertEquals(1, productRepository.findAll().size());
+	}
+
+	@Test
+	@Order(2)
+	void getProductsTest() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/product"))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.length()").value(org.hamcrest.Matchers.greaterThan(0)))
+				.andExpect(jsonPath("$[0].name").value("iphone 13"))
+				.andExpect(jsonPath("$[0].price").value(BigDecimal.valueOf(1200)))
+				.andExpect(jsonPath("$[0].description").value("This is the iPhone 13"));
+	}
+
+	private ProductRequest getProductRequest() {
+		return ProductRequest.builder()
+				.name("iphone 13")
+				.description("This is the iPhone 13")
+				.price(BigDecimal.valueOf(1200))
+				.build();
 	}
 }
 
